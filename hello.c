15cef5d639f163d2f80221c4e644ea362a17659d
Gleb Moisseyev <gmoisseyev2@huskers.unl.edu>
2020-08-12 17:38:12 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Gleb Moiseyev
+ * Date: Aug 12 2020
+ *
+ * First program in C!!!
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv){
+     printf("Hello world!\n");
+
+     return 0;
+ }
